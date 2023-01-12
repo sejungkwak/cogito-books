da529f6da95f2089301d3cf58355a68fe12c8d12
Sejung Kwak <kwak.sejung@gmail.com>
2023-01-12 15:26:14 +0000

    feat: Set up allauth and email

--- cogito_books/settings.py
+++ cogito_books/settings.py
@@ -27,9 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'cogitobooks.herokuapp.com',
+    '127.0.0.1',
+]
 
 
 # Application definition
@@ -41,6 +44,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -70,6 +78,39 @@ TEMPLATES = [
         },
     },
 ]
+
+# Allauth config
+
+SITE_ID = 1
+
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_PREVENT_ENUMERATION = False
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'help@cogitobooks.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_TIMEOUT = 5
+    EMAIL_HOST_USER = os.environ.get('EMAIL_ID')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_KEY')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_ID')
 
 WSGI_APPLICATION = 'cogito_books.wsgi.application'
 
