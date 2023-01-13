325a7c819f2720f3a990c44f7f5175a38ccf4ec4
Sejung Kwak <kwak.sejung@gmail.com>
2023-01-13 19:12:13 +0000

    feat: Add terms & conditions

--- home/urls.py
+++ home/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
         TemplateView.as_view(
             template_name='home/privacy_policy.html'),
         name='privacy_policy'),
+    path('terms_and_conditions/',
+        TemplateView.as_view(
+            template_name='home/terms_conditions.html'),
+        name='terms_conditions'),
 ]
