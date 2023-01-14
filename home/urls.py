687d70d189243153fe3a82ab2b158dc41546bcc8
Sejung Kwak <kwak.sejung@gmail.com>
2023-01-14 17:58:04 +0000

    fix: Fix indentations

--- home/urls.py
+++ home/urls.py
@@ -3,15 +3,15 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('',
-        TemplateView.as_view(
-            template_name='home/index.html'),
-        name='home'),
+         TemplateView.as_view(
+             template_name='home/index.html'),
+         name='home'),
     path('privacy_policy/',
-        TemplateView.as_view(
-            template_name='home/privacy_policy.html'),
-        name='privacy_policy'),
+         TemplateView.as_view(
+             template_name='home/privacy_policy.html'),
+         name='privacy_policy'),
     path('terms_and_conditions/',
-        TemplateView.as_view(
-            template_name='home/terms_conditions.html'),
-        name='terms_conditions'),
+         TemplateView.as_view(
+             template_name='home/terms_conditions.html'),
+         name='terms_conditions'),
 ]
