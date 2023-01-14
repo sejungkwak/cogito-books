fa1c4a9c1c32872713d6939172f4781694f7d42a
Sejung Kwak <kwak.sejung@gmail.com>
2023-01-14 18:02:36 +0000

    feat: Create genre, author, book database model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,72 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+
+class Genre(models.Model):
+    """
+    Genre database model
+    """
+    name = models.CharField(max_length=254, unique=True)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Author(models.Model):
+    """
+    Author database model
+    """
+    full_name = models.CharField(max_length=254)
+
+    class Meta:
+        ordering = ['full_name']
+
+    def __str__(self):
+        return self.full_name
+
+
+class Book(models.Model):
+    """
+    Book database model
+    """
+    genre = models.ForeignKey(
+        Genre,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='books')
+    title = models.CharField(max_length=254)
+    author = models.ManyToManyField(Author, related_name='books')
+    desc = models.TextField()
+    cover_url = models.URLField(max_length=1024, null=True, blank=True)
+    cover = models.ImageField(null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    pages = models.IntegerField()
+    dimension_x = models.FloatField()
+    dimension_y = models.FloatField()
+    dimension_z = models.FloatField()
+    lang = models.CharField(max_length=50, null=True, blank=True)
+    publisher = models.CharField(max_length=254, null=False, blank=False)
+    pub_date = models.DateField()
+    isbn10 = models.CharField(
+        max_length=10, validators=[
+            MinLengthValidator(10)])
+    isbn13 = models.CharField(
+        max_length=13, validators=[
+            MinLengthValidator(13)])
+    discount_rate = models.DecimalField(
+        max_digits=3, decimal_places=2, default=0.00)
+    amount_sold = models.IntegerField()
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
