4f76cb28dccadd554170549b67d60346da341e05
Sejung Kwak <kwak.sejung@gmail.com>
2023-01-15 03:01:56 +0000

    feat: Set up admin panel for book app

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from .models import Genre, Author, Book
 
-# Register your models here.
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    """
+    Genre panel for the admin site
+    """
+    list_display = (
+        'friendly_name',
+    )
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    """
+    Author panel for the admin site
+    """
+    list_display = (
+        'full_name',
+    )
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    """
+    Book panel for the admin site
+    """
+    list_display = (
+        'title',
+        'genre',
+        'all_authors',
+        'price',
+        'amount_sold'
+    )
+
+    def all_authors(self, obj):
+        return ' & '.join([a.full_name for a in obj.author.all()])
