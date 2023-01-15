8e4b1aa76531d500754d812bf387595b2d3fb7c4
Sejung Kwak <kwak.sejung@gmail.com>
2023-01-15 11:49:02 +0000

    feat: Create view to get bestsellers and new releases

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+from . import views
+
 urlpatterns = [
     path('',
-         TemplateView.as_view(
-             template_name='home/index.html'),
+         views.BookListView.as_view(),
          name='home'),
     path('privacy_policy/',
          TemplateView.as_view(
--- home/views.py
+++ home/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
 
-# Create your views here.
+from books.models import Book
+
+
+class BookListView(ListView):
+    """
+    Display a list of bestsellers and new releases.
+    """
+    model = Book
+    template_name = 'home/index.html'
+
+    def get_context_data(self, **kwargs):
+        """
+        Filter books by the number of sales and publication date.
+        """
+        context = super().get_context_data(**kwargs)
+        context['bestsellers'] = Book.objects.order_by('-amount_sold')[:10]
+        context['new_releases'] = Book.objects.order_by('-pub_date')[:10]
+
+        return context
