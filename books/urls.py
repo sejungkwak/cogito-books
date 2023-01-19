1b22f4e5e0455026b38a49fc875119ab5b974ea7
Sejung Kwak <kwak.sejung@gmail.com>
2023-01-19 00:11:34 +0000

    feat: Create book details page url path

--- books/urls.py
+++ books/urls.py
@@ -6,13 +6,16 @@ urlpatterns = [
     path('',
          views.BookListView.as_view(),
          name='book_list'),
+    path('<int:pk>/',
+         views.BookDetailView.as_view(),
+         name='book_detail'),
     path('add_book/',
          views.BookCreateView.as_view(),
          name='add_book'),
-    path('edit_book/<int:pk>',
+    path('edit_book/<int:pk>/',
          views.BookUpdateView.as_view(),
          name='edit_book'),
-    path('delete_book/<int:pk>',
+    path('delete_book/<int:pk>/',
          views.BookDeleteView.as_view(),
          name='delete_book')
 ]
