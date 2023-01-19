26e10c91fc1ef94567dab4325638861e6ed335a9
Sejung Kwak <kwak.sejung@gmail.com>
2023-01-19 00:08:54 +0000

    feat: Create a js function to toggle truncation for book description text

---
+++ books/static/books/js/truncate_toggler.js
@@ -0,0 +1,10 @@
+/**----------------------------------------------------------------------------
+ * Toggles the truncated book description.
+ * Runs when the user clicks the 'Read more/less' button on the details page.
+ -----------------------------------------------------------------------------*/
+$(document).ready(function() {
+    $('.show-hide-btn').click(function() {
+        $('.half-desc').toggle();
+        $('.full-desc').toggle();
+      })
+})
