b68ba72365f1cc6067c53b229747c6b8ca49a14d
Sejung Kwak <kwak.sejung@gmail.com>
2023-01-19 00:06:02 +0000

    chore: Add docstring to sort_option.js

--- books/static/books/js/sort_option.js
+++ books/static/books/js/sort_option.js
@@ -1,21 +1,26 @@
-$('#sort-selector').change(function() {
-    let selector = $(this);
-    let currentUrl = new URL(window.location);
-
-    let selectedVal = selector.val();
-
-    if ( selectedVal != 'reset' ) {
-        let sort = selectedVal.split('-')[0];
-        let direction = selectedVal.split('-')[1];
-
-        currentUrl.searchParams.set('sort', sort);
-        currentUrl.searchParams.set('direction', direction);
-
-        window.location.replace(currentUrl);
-    } else {
-        currentUrl.searchParams.delete('sort');
-        currentUrl.searchParams.delete('direction');
-
-        window.location.replace(currentUrl);
-    }
+/**----------------------------------------------------------------------------
+ * Changes the url parameter when the user selects an option in the dropbox
+ * on the book list page.
+ -----------------------------------------------------------------------------*/
+$(document).ready(function() {
+    $('#sort-selector').change(function() {
+        let selector = $(this);
+        let currentUrl = new URL(window.location);
+        let selectedVal = selector.val();
+    
+        if ( selectedVal != 'reset' ) {
+            let sort = selectedVal.split('-')[0];
+            let direction = selectedVal.split('-')[1];
+    
+            currentUrl.searchParams.set('sort', sort);
+            currentUrl.searchParams.set('direction', direction);
+    
+            window.location.replace(currentUrl);
+        } else {
+            currentUrl.searchParams.delete('sort');
+            currentUrl.searchParams.delete('direction');
+    
+            window.location.replace(currentUrl);
+        }
+    })
 })
