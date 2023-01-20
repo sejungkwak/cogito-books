d5c0bbb6422126f4487e02a6d724f78bf2dcfdf2
Sejung Kwak <kwak.sejung@gmail.com>
2023-01-20 23:27:13 +0000

    feat: Create Order and OrderLineItem models for admin site

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    """
+    The OrderLineItem section for the order panel.
+    """
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    """
+    The Order panel for the admin site.
+    """
+    inlines = (OrderLineItemAdminInline,)
+    readonly_fields = (
+        'order_number',
+        'date',
+        'delivery_cost',
+        'subtotal',
+        'paid_points',
+        'grand_total')
+    fields = (
+        'order_status',
+        'order_number',
+        'date',
+        'full_name',
+        'email',
+        'phone_number',
+        'country',
+        'postcode',
+        'town_or_city',
+        'address_line_1',
+        'address_line_2',
+        'county',
+        'delivery_cost',
+        'subtotal',
+        'paid_points',
+        'grand_total')
+    list_display = (
+        'order_status',
+        'order_number',
+        'date',
+        'full_name',
+        'subtotal',
+        'delivery_cost',
+        'grand_total')
