678a5fc5cb904e3fb76699c5585fe536ab6ebd60
Sejung Kwak <kwak.sejung@gmail.com>
2023-01-20 17:37:04 +0000

    feat: Add alert tags and js function to close alert

--- cogito_books/settings.py
+++ cogito_books/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 import os
 if os.path.isfile('env.py'):
@@ -70,6 +71,14 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'cogito_books.urls'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger'
+}
 
 TEMPLATES = [
     {
---
+++ static/js/script.js
@@ -0,0 +1,13 @@
+// Close the alert automatically after 3 seconds.
+$(document).ready(function() {
+    setTimeout(function() {
+        $('#msg').alert('close');
+    }, 3000);
+});
+
+// Allauth alert styling
+$(document).ready(function() {
+    $('.login .alert-error').addClass('alert-danger');
+    $('.login .alert-error ul').css({'list-style': 'none', 'margin': 0, 'padding': 0});
+    $('.signup .help-block').addClass('alert alert-danger');
+});
