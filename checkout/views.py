159948eb20670473bed0590388193284860f4f07
Sejung Kwak <kwak.sejung@gmail.com>
2023-01-21 16:12:36 +0000

    feat: Create checkout template and view

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .models import Order
+from .forms import OrderForm
+
+
+def checkout(request):
+    """
+    A view to handle the checkout-related request.
+    """
+    basket = request.session.get('basket', {})
+    if not basket:
+        messages.error(request, "Your basket is currently empty.")
+        return redirect(reverse('book_list'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form
+    }
+
+    return render(request, template, context)
