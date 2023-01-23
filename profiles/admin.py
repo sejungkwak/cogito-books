7e7fbbd0731f7dfc42a3d63630564060330d519c
Sejung Kwak <kwak.sejung@gmail.com>
2023-01-23 08:47:13 +0000

    feat: Create profiles panel for admin site

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django import forms
+from .models import Profile
+from phonenumber_field.widgets import PhoneNumberPrefixWidget
 
-# Register your models here.
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        widgets = {
+            'default_phone_number': PhoneNumberPrefixWidget(initial='IE'),
+        }
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    """
+    Profile view in the admin panel
+    """
+    model = Profile
+    form = ProfileForm
+    list_display = ('id', 'username', 'dob', 'joined', 'loyalty_points')
+
+    def id(self, obj):
+        return obj.id
+
+    def username(self, obj):
+        return obj.user.username
