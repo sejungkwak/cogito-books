053e78f1944aaeac56e00feca522e1fbada8b479
Sejung Kwak <kwak.sejung@gmail.com>
2023-01-26 14:00:08 +0000

    feat: Register contact model to admin site

--- helpcentre/admin.py
+++ helpcentre/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import UserContact
 
-# Register your models here.
+
+@admin.register(UserContact)
+class UserContactModel(admin.ModelAdmin):
+    """
+    The UserContact panel for the admin site.
+    """
+    model = UserContact
+    list_display = [field.name for field in UserContact._meta.get_fields()]
