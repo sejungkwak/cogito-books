181a598154aa42e5a9dab5ab721e390ec6bdb9b5
Sejung Kwak <kwak.sejung@gmail.com>
2023-01-26 01:11:50 +0000

    feat: Create custom http error handler pages

---
+++ cogito_books/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+
+
+def handler404(request, exception):
+    """
+    Error Handler 404 - Page Not Found
+    """
+    return render(request, 'errors/404.html', status=404)
+
+
+def handler500(request, *args, **argv):
+    """
+    Error Handler 500 - Server Error
+    """
+    return render(request, 'errors/500.html', status=500)
+
+
+def handler403(request, exception):
+    """
+    Error Handler 403 - HTTP Forbidden / Permission denied
+    """
+    return render(request, 'errors/403.html', status=403)
+
+
+def handler400(request, exception):
+    """
+    Error Handler 400 - Bad Request
+    """
+    return render(request, 'errors/400.html', status=400)
