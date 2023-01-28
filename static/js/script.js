2dc4223b5e88e2766b82c04245b68bba1ba60b2e
Sejung Kwak <kwak.sejung@gmail.com>
2023-01-28 17:27:15 +0000

    fix: Fix bug in mobile nav not auto-closing

--- static/js/script.js
+++ static/js/script.js
@@ -11,3 +11,14 @@ $(document).ready(function() {
     $('.login .alert-error ul').css({'list-style': 'none', 'margin': 0, 'padding': 0});
     $('.signup .help-block').addClass('alert alert-danger');
 });
+
+// Fix for the mobile dropdown menu not auto-closing issue.
+// Hide dropdown menu if another element is selected if the menu is open.
+$(document).ready(function() {
+    $(document).on('click', function(e) {
+        let dropdownMainMenu = $('#dropdownMainMenu');
+        if (!dropdownMainMenu.is(e.target) && dropdownMainMenu.has(e.target).length === 0 && dropdownMainMenu.hasClass('show')) {
+            dropdownMainMenu.removeClass('show');
+        }
+    });
+});
